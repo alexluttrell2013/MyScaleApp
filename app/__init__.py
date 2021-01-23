f38aa76d47323dbbb795f80cb3531df48d73de41
Alex Luttrell <alexluttrell2013@gmail.com>
2021-01-23 12:01:43 -0800

    Added hx711 read on server side

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from config import Config
+from hx711 import HX711
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+hx = HX711(2,3)
 
 from app import routes, models
